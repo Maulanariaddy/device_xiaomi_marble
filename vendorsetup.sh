70e1684d9e3998667ebc67ce246c0eb7cb80bc56
mauls-xyz <maulriaddy23@gmail.com>
2023-10-04 19:05:21 +0700

    marble: auto clone tree

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,7 +3,7 @@ echo 'Starting to clone stuffs needed to build for marble'
 
 # Vendor
 echo 'Cloning vendor tree'
-rm -rf vendor/xiaomi/marble && git clone https://github.com/SMGLaboratory/vendor_xiaomi_marble.git vendor/xiaomi/marble
+rm -rf vendor/xiaomi/marble && git clone https://github.com/maul-s/vendor_xiaomi_marble.git vendor/xiaomi/marble
 
 
 # Kernel
@@ -22,7 +22,11 @@ git clone https://gitlab.com/vherawidatama/proprietary_vendor_xiaomi_camera.git 
 
 # Hardware Xiaomi
 echo 'Cloning Hardware Xiaomi'
-rm -rf hardware/xiaomi && git clone https://github.com/SMGLaboratory/hardware_xiaomi.git hardware/xiaomi
+rm -rf hardware/xiaomi && git clone https://github.com/maul-s/hardware_xiaomi.git hardware/xiaomi
+
+# BCR tree
+echo 'Cloning BCR Tree'
+git clone https://github.com/maul-s/vendor_bcr.git vendor/bcr
 
 
 echo 'delete vendorsetup.sh from device tree once this is done'
