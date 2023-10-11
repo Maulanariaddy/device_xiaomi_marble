71655659497737e64977cbf5be8a9633cdf5149c
YuKongA <70465933+YuKongA@users.noreply.github.com>
2023-10-11 16:16:57 +0800

    marble: import scripts from stock
    * Refer to https://github.com/cupid-development/android_device_xiaomi_sm8450-common/tree/lineage-20/rootdir

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -41,11 +41,20 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.recovery.qcom.rc
+LOCAL_MODULE       := charger_fw_fstab.qti
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.recovery.qcom.rc
-LOCAL_MODULE_PATH  := $(TARGET_RECOVERY_ROOT_OUT)
+LOCAL_SRC_FILES    := etc/charger_fw_fstab.qti
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := ueventd-odm.rc
+LOCAL_MODULE_STEM  := ueventd.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/ueventd-odm.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ODM)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
