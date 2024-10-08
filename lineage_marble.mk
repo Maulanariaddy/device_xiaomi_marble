6b26238a9e574cd4b5b62ec728282b32bed6a47d
Fiqri Ardyansyah <fiqri191002@gmail.com>
2024-10-08 12:10:21 +0700

    marble: Update blobs and firmware from V816.0.9.0.UMRMIXM
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- lineage_marble.mk
+++ lineage_marble.mk
@@ -23,6 +23,6 @@ PRODUCT_DEVICE := marble
 PRODUCT_MANUFACTURER := Xiaomi
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := POCO/marble_global/marble:14/UKQ1.230804.001/V816.0.8.0.UMRMIXM:user/release-keys
+BUILD_FINGERPRINT := POCO/marble_global/marble:14/UKQ1.230804.001/V816.0.9.0.UMRMIXM:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
