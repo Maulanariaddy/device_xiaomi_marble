d09d6b77c05d5fb83c0d6e7631859599f91590fa
mauls-xyz <maulriaddy23@gmail.com>
2024-10-27 21:58:35 +0700

    marble: Afterlife Never Die
    Change-Id: Ic364b17432a6d575a0d7241837e679a14aa6c773

---
+++ afterlife_marble.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2022-2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from marble device
+$(call inherit-product, device/xiaomi/marble/device.mk)
+
+# Inherit from common Afterlife configuration
+$(call inherit-product, vendor/afterlife/config/common_full_phone.mk)
+
+# Afterlife Build stuff
+AFTERLIFE_MAINTAINER := MAULSxSMG
+TARGET_BOOT_ANIMATION_RES := 1440
+TARGET_SUPPORTS_BLUR := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Flags Afterlife
+AFTERLIFE_MAINTANER := true
+AFTERLIFE_BUILD_TYPE = OFFICIAL
+
+PRODUCT_NAME := afterlife_marble
+PRODUCT_DEVICE := marble
+PRODUCT_MANUFACTURER := Xiaomi
+
+BUILD_FINGERPRINT := POCO/marble_global/marble:14/UKQ1.230804.001/V816.0.9.0.UMRMIXM:user/release-keys
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
