ee102883a106de9a88d01660946fc113713ae85f
Maulana Riaddy <maulriaddy@gmail.com>
2025-01-20 11:43:15 +0700

    marble: Welcome Skyline-15

---
+++ vendorsetup.sh
@@ -0,0 +1,29 @@
+echo 'Hello! we need to clone stuffs needed for your device, please wait tll process complete'
+echo 'Nuke Source stuffs'
+	rm -rf hardware/qcom-caf/sm8450/display
+	rm -rf hardware/xiaomi
+
+echo 'Cloning Basic Call Recorder Tree'
+	git clone https://github.com/xiaomi-sm8450-marble/android_vendor_bcr vendor/bcr
+
+echo 'Cloning Device Common Tree'
+	git clone hhttps://github.com/Maulanariaddy/device_xiaomi_marble_sm8450_common.git -b common-skyline device/xiaomi/sm8450-common
+
+echo 'Cloning MemeCamera Tree'
+	git clone https://github.com/marble-indonesia/device_xiaomi_miuicamera-marble.git -b vic-miuicam device/xiaomi/miuicamera-marble
+	git clone https://gitlab.com/xiaomi-sm8450-marble/android_vendor_xiaomi_miuicamera-marble.git vendor/xiaomi/miuicamera-marble
+
+echo 'Cloning Hardware Display Tree'
+	git clone https://github.com/marble-indonesia/hardware_qcom-caf_sm8450_display.git -b vic-display hardware/qcom-caf/sm8450/display
+
+echo 'Cloning Hardware Xiaomi Tree'
+	git clone https://github.com/Maulanariaddy/hardware_xiaomi.git -b skyline hardware/xiaomi
+
+echo 'Cloning Kernel Tree'
+	git clone https://github.com/marble-indonesia/device_xiaomi_marble-kernel.git device/xiaomi/marble-kernel
+
+echo 'Cloning Vendor Tree'
+	git clone https://github.com/Maulanariaddy/vendor_xiaomi_marble.git -b skyline-15 vendor/xiaomi/marble
+
+echo 'Cloning Vendor Common Tree'
+	git clone https://github.com/Maulanariaddy/vendor_xiaomi_sm8450_common.git -b common-skyline vendor/xiaomi/sm8450-common
